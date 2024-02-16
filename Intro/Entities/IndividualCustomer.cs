6bfdc237142d6aaac0853b8aa2690f57ed21ef26
sevgiyilmaz <sevgiyilmaz2112@gmail.com>
2024-02-16 23:55:13 +0300

    polymorphism kullanıldı

---
+++ Intro/Entities/IndividualCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Entities;
+
+public class IndividualCustomer : BaseCustomer
+{
+    public string FİrstName { get; set; }
+    public string LastName { get; set; }
+    public string NatıonalIdentity { get; set; }
+
+}
--- Intro/obj/Debug/net8.0/Intro.AssemblyInfo.cs
+++ Intro/obj/Debug/net8.0/Intro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf830b921e01f0745e9cb8cdc5878556a6031032")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
